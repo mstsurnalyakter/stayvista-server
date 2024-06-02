9aca19c191c9610b7c74a7730a41626d9eb0bf58
mstsurnalyakter <surnalyms@gmail.com>
2024-06-02 12:15:34 +0600

    Statistics page functionality added

--- index.js
+++ index.js
@@ -389,10 +389,10 @@ async function run() {
 
     // Host Statistics
     app.get("/host-stat", verifyToken, verifyHost, async (req, res) => {
-      const {email} = req.user;
+      const { email } = req.user;
       const bookingDetails = await bookingsCollection
         .find(
-          {"host.email":email},
+          { "host.email": email },
           {
             projection: {
               date: 1,
@@ -409,11 +409,10 @@ async function run() {
         (sum, booking) => sum + booking.price,
         0
       );
-
-      const {timestamp} = await usersCollection.findOne(
-        {email},
-        {projection:{timestamp:1}}
-      )
+      const { timestamp } = await usersCollection.findOne(
+        { email },
+        { projection: { timestamp: 1 } }
+      );
 
       const chartData = bookingDetails.map((booking) => {
         const day = new Date(booking.date).getDate();
@@ -428,11 +427,56 @@ async function run() {
 
       console.log(bookingDetails);
       res.send({
-        hostSince:timestamp,
         totalRooms,
         totalBookings: bookingDetails.length,
         totalPrice,
         chartData,
+        hostSince: timestamp,
+      });
+    });
+
+
+    // guest Statistics
+    app.get("/guest-stat", verifyToken, async (req, res) => {
+      const { email } = req.user;
+      const bookingDetails = await bookingsCollection
+        .find(
+          { "guest.email": email },
+          {
+            projection: {
+              date: 1,
+              price: 1,
+            },
+          }
+        )
+        .toArray();
+
+      const totalPrice = bookingDetails.reduce(
+        (sum, booking) => sum + booking.price,
+        0
+      );
+      const { timestamp } = await usersCollection.findOne(
+        { email },
+        { projection: { timestamp: 1 } }
+      );
+
+      const chartData = bookingDetails.map((booking) => {
+        const day = new Date(booking.date).getDate();
+        const month = new Date(booking.date).getMonth() + 1;
+        const data = [`${day}/${month}`, booking?.price];
+        return data;
+      });
+      chartData.unshift(["Day", "Sales"]);
+      // chartData.splice(0, 0, ['Day', 'Sales'])
+
+      console.log(chartData);
+
+      console.log(bookingDetails);
+      res.send({
+        totalBookings: bookingDetails.length,
+        totalPrice,
+        chartData,
+        guestSince: timestamp,
       });
     });
 
